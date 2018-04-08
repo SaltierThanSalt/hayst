ab565f5955ceeb37ded69f0b956f295a70a06047
mikey1384 <mikey1384@gmail.com>
2018-04-08 20:16:43 +0700

    message board added

---
+++ src/containers/Home/Main/MsgBoard.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { css } from 'emotion';
+import request from 'axios';
+import URL from '../../../constants/URL';
+
+export default class MsgBoard extends Component {
+  state = {
+    inputText: '',
+    msgs: []
+  };
+
+  async componentDidMount() {
+    const { data } = await request.get(`${URL}/posts`);
+    this.setState({
+      msgs: data
+    });
+  }
+
+  render() {
+    const { inputText, msgs } = this.state;
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1
+          className={css`
+            margin-top: 1rem;
+          `}
+        >
+          Here Is A Place You Can Chat.{' '}
+        </h1>
+        <input
+          value={inputText}
+          onChange={event => this.setState({ inputText: event.target.value })}
+          placeholder="Ex) Hello There! General Kenobi!"
+          style={{
+            width: '50%',
+            marginLeft: '25%',
+            marginTop: '2em'
+          }}
+          onKeyUp={event => {
+            if (event.key === 'Enter') {
+              this.onSubmit();
+            }
+          }}
+          className="form-control"
+        />
+        <div
+          className={css`
+            padding: 1rem;
+            font-size: 2.5rem;
+          `}
+        >
+          {msgs.map(msg => (
+            <div key={msg.id}>
+              {msg.content}{' '}
+              <button onClick={() => this.onDelete(msg.id)}>delete</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  onDelete = async postId => {
+    await request.delete(`${URL}/posts?id=${postId}`);
+    this.setState(state => ({
+      msgs: state.msgs.filter(msg => msg.id !== postId)
+    }));
+  };
+
+  onSubmit = async () => {
+    const { inputText } = this.state;
+    const { data } = await request.post(`${URL}/posts`, { post: inputText });
+    this.setState(state => ({
+      inputText: '',
+      msgs: state.msgs.concat(data)
+    }));
+  };
+}
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Greeting from "./Greeting";
 import Thanks from "./Thanks";
-import Preview from "./Preview";
+import Main from "./Main";
 
 export default function Home() {
   return (
     <div>
       <Greeting />
       <Thanks />
-      <Preview />
+      <Main />
     </div>
   );
 }
